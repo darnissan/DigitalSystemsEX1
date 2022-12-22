85095d9db8d0c5037f42f4cee32f719aea4e5a99
darnissan <67154616+darnissan@users.noreply.github.com>
2022-12-22 18:09:18 +0200

    fas_test updated

--- fas.sv
+++ fas.sv
@@ -13,55 +13,95 @@ module fas (
 
 	logic g1,g2,g3,g4,g5,g6,g7,g8;
 // End of your code
-	XNOR2 xor2_Aa_ns (
+	XNOR2#(
+        .Tpdlh(9),
+        .Tpdhl(9)
+		
+		)xor2_Aa_ns (
         .Z(g1),
         .A(a),
         .B(a_ns)
     );
-	XNOR2 xor2_BCin (
+	XNOR2#(
+        .Tpdlh(9),
+        .Tpdhl(9)
+		
+		) xor2_BCin (
         .Z(g2),
         .A(b),
         .B(cin)
     );
-	NAND2 nand2_BCin1 (
+	NAND2#(
+        .Tpdlh(5),
+        .Tpdhl(5)
+		
+		) nand2_BCin1 (
         .Z(g3),
         .A(b),
         .B(cin)
     );
 	
-	NAND2 nand2_BCin2 (
+	NAND2#(
+        .Tpdlh(5),
+        .Tpdhl(5)
+		
+		) nand2_BCin2 (
         .Z(g4),
         .A(g3),
         .B(g3)
     );
 	
-	NAND2 nand2_BxorC (
+	NAND2#(
+        .Tpdlh(5),
+        .Tpdhl(5)
+		
+		) nand2_BxorC (
         .Z(g5),
         .A(g2),
         .B(g2)
     );
 	
-	NAND2 nand2_BxorCAxnorA_ns (
+	NAND2#(
+        .Tpdlh(5),
+        .Tpdhl(5)
+		
+		) nand2_BxorCAxnorA_ns (
         .Z(g6),
         .A(g1),
         .B(g5)
     );
-	NAND2 nand2_BxorCAxnorA_ns2 (
+	NAND2 #(
+        .Tpdlh(5),
+        .Tpdhl(5)
+		
+		)nand2_BxorCAxnorA_ns2 (
         .Z(g7),
         .A(g6),
         .B(g6)
     );
-	OR2 or2_orinst(
+	OR2#(
+        .Tpdlh(7),
+        .Tpdhl(7)
+		
+		) or2_orinst(
 		.Z(cout),
 		.A(g7),
 		.B(g4)
 	);
-	XNOR2 xor2_AXnorB (
+	XNOR2#(
+        .Tpdlh(9),
+        .Tpdhl(9)
+		
+		) xor2_AXnorB (
         .Z(g8),
         .A(b),
         .B(a)
     );
-	XNOR2 xnor2_ABCIN(
+	XNOR2#(
+        .Tpdlh(9),
+        .Tpdhl(9)
+		
+		) xnor2_ABCIN(
 	.Z(s),
 	.A(cin),
 	.B(g8)
--- fas_test.sv
+++ fas_test.sv
@@ -5,6 +5,66 @@ module fas_test;
 // ------------------
 
 
+
+// Put your code here
+// ------------------
+
+	logic fas_a;          // Data input 0
+    logic fas_b;          // Data input 1
+    logic fas_cin;         // Select input
+    logic fas_Ans;	// Output
+	logic fas_s;
+	logic fas_cout;
+
+	fas uut (
+		.a(fas_a),
+		.b(fas_b),
+		.cin(fas_cin),
+		.a_ns(fas_Ans),
+		.s(fas_s),
+		.cout(fas_cout)
+	);
+	initial begin
+//b=0->1 a=0 cin=0 a_ns=0 path
+		fas_a= 1'b0;
+		fas_b = 1'b0;
+		fas_Ans = 1'b0;
+		fas_cin=1'b0;
+		
+		#100;
+		fas_a= 1'b0;
+		fas_b = 1'b1;
+		fas_Ans = 1'b0;
+		fas_cin=1'b0;
+		#100;
+		fas_a= 1'b0;
+		fas_b = 1'b0;
+		fas_Ans = 1'b0;
+		fas_cin=1'b0;
+		#200
+		
+		//a=0->1 b=1 cin=0 a_ns=0 path
+		fas_a= 1'b0;
+		fas_b = 1'b1;
+		fas_Ans = 1'b0;
+		fas_cin=1'b0;
+		
+		#100;
+		fas_a= 1'b1;
+		fas_b = 1'b1;
+		fas_Ans = 1'b0;
+		fas_cin=1'b0;
+		#100;
+		fas_a= 1'b0;
+		fas_b = 1'b1;
+		fas_Ans = 1'b0;
+		fas_cin=1'b0;
+		
+		
+		end
+		
+		
+		
 // End of your code
 
 endmodule
