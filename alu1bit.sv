c91fd8b76e0b33babdfdad88bd37a075e6209ff0
suissaadi <adi.suissa@campus.technion.ac.il>
2022-12-26 14:31:07 +0200

    Update alu1bit.sv

--- alu1bit.sv
+++ alu1bit.sv
@@ -8,10 +8,74 @@ module alu1bit (
     output logic cout        // Carry out
 );
 
-// Put your code here
-// ------------------
-
-
-// End of your code
-
+	logic w0, w1, w2, w3, w4, w5;
+	
+	or2#(
+        .Tpdlh(7),
+        .Tpdhl(7)
+		
+		) or_nor (
+        .z(w4),
+        .a(a),
+        .b(b)
+    );
+	
+	nand2#(
+        .Tpdlh(5),
+        .Tpdhl(5)
+		
+		) nand_nor (
+        .z(w0),
+        .a(w4),
+        .b(w4)
+    );
+	
+	xnor2# (
+		.Tpdlh(9), 
+		.Tpdhl(9)
+		
+		)xnor_xor(
+		.z(w5), 
+		.a(a), 
+		.b(b)
+	);
+	
+	nand2# (
+		.Tpdlh(5), 
+		.Tpdhl(5)
+		
+		) nand_xor(
+		.z(w1), 
+		.a(w5), 
+		.b(w5)
+	);
+	
+	nand2# (
+		.Tpdlh(5), 
+		.Tpdhl(5)
+		
+		) nand_fas(
+		.z(w3), 
+		.a(op[0]), 
+		.b(op[0])
+	);
+	
+	fas ALU_fas (
+		.a(a), 
+		.b(b), 
+		.cin(cin), 
+		.a_ns(w3), 
+		.s(w2), 
+		.cout(cout)
+	);
+	
+	mux4 ALU_mux4 (
+		.d0(w0), 
+		.d1(w1), 
+		.d2(w2), 
+		.d3(w3), 
+		.sel(op), 
+		.z(s)
+	);
+	
 endmodule
